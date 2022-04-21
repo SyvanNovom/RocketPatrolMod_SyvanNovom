5af3c03c204d47fd0cfbee80ee37797570f3e07b
SethNovom <59626121+SethNovom@users.noreply.github.com>
2022-04-20 18:48:38 -0700

    Personal Score
    Name, date, etc. in README
    Personal score in main.js

--- src/main.js
+++ src/main.js
@@ -15,3 +15,13 @@ let borderPadding = borderUISize / 3;
 let shoot, keyR, keyA, keyD;
 
 let highscore = 0;
+
+// High Score (5)
+// Fire UI text (5)
+// Scrolling Tile Sprite for background (5) - Implemented as part of parallax scrolling
+// New Title Screen (10)
+// Implemented parallax scrolling (10)
+// Created new smaller spaceship type (20)
+// Created new artwork for all in game assets (20)
+// Created and implemented new weapon w/new behavior and graphics (20) - New bahvior is that it does not move, but the laser beam moves very fast, making the game more reaction based
+// Total: 95
